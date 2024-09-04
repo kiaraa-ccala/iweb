095ca33e7faf9dfa8b3135068263f93b3b63fdcd
kiaraa-ccala <142847329+kiaraa-ccala@users.noreply.github.com>
2024-09-04 18:38:08 -0500

    Create clase_celular.java

---
+++ clase_celular.java
@@ -0,0 +1,19 @@
+public class Celular {
+    private String marca;
+    private String modelo;
+    private String color;
+    private String cantidadMemoria;
+    private String sistemaOperativo;
+
+    // Constructor y Getters y Setters
+    public Celular(String marca, String modelo, String color, String cantidadMemoria, String sistemaOperativo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.cantidadMemoria = cantidadMemoria;
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    // Métodos get y set para cada atributo
+    // ...
+}
