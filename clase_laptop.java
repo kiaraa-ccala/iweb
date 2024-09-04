28f132150a97cc119c49ab83edf2f804682e033d
kiaraa-ccala <142847329+kiaraa-ccala@users.noreply.github.com>
2024-09-04 18:37:45 -0500

    Create clase_laptop.java

---
+++ clase_laptop.java
@@ -0,0 +1,23 @@
+public class Laptop {
+    private String marca;
+    private String modelo;
+    private double tamañoPantalla;
+    private String procesador;
+    private String tamañoDisco;
+    private String cantidadMemoria;
+    private String sistemaOperativo;
+
+    // Constructor y Getters y Setters
+    public Laptop(String marca, String modelo, double tamañoPantalla, String procesador, String tamañoDisco, String cantidadMemoria, String sistemaOperativo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tamañoPantalla = tamañoPantalla;
+        this.procesador = procesador;
+        this.tamañoDisco = tamañoDisco;
+        this.cantidadMemoria = cantidadMemoria;
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    // Métodos get y set para cada atributo
+    // ...
+}
